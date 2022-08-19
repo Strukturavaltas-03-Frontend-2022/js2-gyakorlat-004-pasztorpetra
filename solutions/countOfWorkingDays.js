85512ba88680cd5080e50e3b234825ff45bcad82
Petra <pasztor.petra1998@gmail.com>
2022-08-19 17:13:18 +0200

    kész

--- solutions/countOfWorkingDays.js
+++ solutions/countOfWorkingDays.js
@@ -1,12 +1,15 @@
-const countOfWorkingDays = (from, to) => {
-  const startDate = new Date(from);
-  const endDate = new Date(to);
-
-  if (startDate > endDate) {
-    throw new Error('The first parameter is earlier date, than second!');
-  } else {
-    return (Date.now(endDate) - Date.now(startDate)) / 1000 / 60 / 60 / 24 / (7 / 5);
+const countOfWorkingDays = (startDate, endDate) => {
+  let numWorkDays = 0;
+  let currentDate = new Date(startDate);
+  const message = 'The first parameter is earlier date, than second!';
+  while (currentDate <= endDate) {
+    if (currentDate.getDay() !== 0 && currentDate.getDay() !== 6) {
+      numWorkDays++;
+    } else {
+      throw new Error(message);
+    }
+    currentDate = currentDate.addDays(1);
   }
+  return numWorkDays;
 };
-
 export default countOfWorkingDays;
